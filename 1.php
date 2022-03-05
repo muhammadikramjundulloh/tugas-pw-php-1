192ced37d56aae06b14fed2523d2d394397920b6
unknown <you@example.com>
2022-03-05 07:33:18 +0700

    first commit

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+    $jari = 50;
+
+    define('phi',3.14);
+
+    $hasil = phi* $jari*$jari;
+
+
+    echo  "luas lingkaran adalah $hasil";
+    
+    $namabuah = ['cabe','terong','apel','naga'];
+    foreach($namabuah as $buah)
+    echo $buah.'<br>';
+
+
+?>
+
